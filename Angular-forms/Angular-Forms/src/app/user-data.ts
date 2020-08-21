546f8998ceae7813d9d3aee4620c85e8cd6925ce
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-21 21:45:46 +0530

    Create user-data.ts

---
+++ Angular-forms/Angular-Forms/src/app/user-data.ts
@@ -0,0 +1,12 @@
+export class UserData {
+    constructor(
+        public fname:string,
+        public lname:string,
+        public email:string,
+        public mobile:string,
+        public username:string,
+        public password:string,
+        public conpassword:string,
+
+    ){}
+}
