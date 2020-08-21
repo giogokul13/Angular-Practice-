f6f7dd8e176be33fd46740e7b0f9a89d805899d0
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-21 21:45:34 +0530

    Update app.component.ts

--- Angular-forms/Angular-Forms/src/app/app.component.ts
+++ Angular-forms/Angular-Forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {UserData} from './user-data'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Forms';
+
+  User = new UserData("","","",'',"","","");
+  
+  onsubmit(){
+    console.log(this.User)
+  }
 }
