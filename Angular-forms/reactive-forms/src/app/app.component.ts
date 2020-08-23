2d817b7a787e671fa25194bd32dc79192eb055eb
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-23 21:31:12 +0530

    reactive forms

--- Angular-forms/reactive-forms/src/app/app.component.ts
+++ Angular-forms/reactive-forms/src/app/app.component.ts
@@ -1,16 +1,47 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms'
+import {FormControl, FormGroup,FormBuilder,Validator, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
 
-  regformgroup = new FormGroup({
-    uname :new FormControl(),
-    password : new FormControl(),
-    confirmpassword : new FormControl()
+  get uname(){
+    return this.regformgroup.get('uname');
+  }
+  get pwd(){
+    return this.regformgroup.get('password');
+  }
+  get pwd1(){
+    return this.regformgroup.get('confirmpassword');
+  }
+
+
+  title = 'reactive-forms';
+  constructor(private formbuilder : FormBuilder){}
+  // regformgroup = new FormGroup({
+  //   uname :new FormControl('gokul'),
+  //   password : new FormControl(),
+  //   confirmpassword : new FormControl()
+  // });
+
+  //loaddata(){
+  //   this.regformgroup.setValue({
+  //     uname : "gokul",
+  //     password : "12234245",
+  //     confirmpassword : "1236124634"
+  //   })
+  // }
+
+  // this.regformgroup.patchValue({
+  //   uname : "gokul v 2.0"
+  // })
+
+  regformgroup = this.formbuilder.group({
+    uname : ['',[Validators.required,Validators.minLength(5)]],
+    password : ['',[Validators.required,Validators.minLength(8)]],
+    confirmpassword : ['',[Validators.required,Validators.minLength(8)]]
   });
+
 }
